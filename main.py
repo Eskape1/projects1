cb67bb30fadcc140914fb57f6e8682b284f4f024
Eskape1 <danyagamievo@gmail.com>
2025-04-19 19:42:23 +0200

    fics bags, add time

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from time import sleep
 import json
 import os
+from datetime import datetime
+
 
 class Pomidor:
 
-    def __init__(self, user_name, job_time=25, pause_time=5):
+    def __init__(self, user_name, job_time=25, pause_time=5):  #standart values without configuration
         self.user_name = user_name
         self.job_time = job_time
         self.pause_time = pause_time
@@ -12,7 +14,7 @@ class Pomidor:
         self.count_of_sessions = 0
         self.history = 'history.json'
 
-    def time_config(self):
+    def time_config(self):  #configuration for time of job and break
         if input('Do you want to configurate time? [y/n]: ').lower() == 'y':
             self.job_time = int(input("How many minutes will you work: "))
             self.pause_time = int(input("How many minutes will you relax: "))
@@ -26,32 +28,27 @@ class Pomidor:
         print("Pause")
         sleep(self.pause_time * self._minute)
 
-    def check_history(self):
-        if os.path.exists(self.history) and os.path.getsize(self.history) > 0:
-            with open(self.history, "r") as file:
-                try:
-                    data = json.load(file)
-                    return data
-                except json.JSONDecodeError:
-                    data = {}
-                    return data
-
-        else:
-            data = {}
-            return data
+    def check_history(self):  #func checks file exist. if not - creates new. Return data from file
+        if not os.path.exists(self.history) or os.path.getsize(self.history) == 0:
+            return {}
+        with open(self.history, "r") as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return {}
 
     def save_history(self):
         data = self.check_history()
-        try:
-            if data[self.user_name] in data.values():
-                data[self.user_name] += self.count_of_sessions
-                with open(self.history, 'w') as file:
-                    json.dump(data, file, indent=4)
-        except KeyError:
-            with open(self.history, 'w') as file:
-                data[self.user_name] = self.count_of_sessions
-                json.dump(data, file, indent=4)
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+        if self.user_name in data:
+            data[self.user_name][0] += self.count_of_sessions
+            data[self.user_name][1] = now
+        else:
+                data[self.user_name] = {'Session': self.count_of_sessions, 'Time':now}
+            
+        with open(self.history, 'w') as file:
+            json.dump(data, file, indent=4)
 
     def session(self):
         is_running = True
