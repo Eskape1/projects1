6015cfe7ef25b0991c8cc25626ebc6b4c0a1521e
Eskape1 <danyagamievo@gmail.com>
2025-04-19 22:14:05 +0200

    added log.json with start time & end time

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from time import sleep
 import json
 import os
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 class Pomidor:
 
@@ -10,9 +9,12 @@ class Pomidor:
         self.user_name = user_name
         self.job_time = job_time
         self.pause_time = pause_time
-        self._minute = 2
+        self._minute = 60
         self.count_of_sessions = 0
-        self.history = 'history.json'
+        self.history = 'history.json'  #short data
+        self.log = 'log.json'  #data with start time, end time(more info)
+        self.start_time = ''
+        self.end_time = ''
 
     def time_config(self):  #configuration for time of job and break
         if input('Do you want to configurate time? [y/n]: ').lower() == 'y':
@@ -37,33 +39,75 @@ class Pomidor:
             except json.JSONDecodeError:
                 return {}
 
+    def check_log(self):  #func checks file exist. if not - creates new. Return data from file
+        if not os.path.exists(self.log) or os.path.getsize(self.log) == 0:
+            return {}
+        with open(self.log, "r") as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return {}
+
     def save_history(self):
         data = self.check_history()
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         if self.user_name in data:
-            data[self.user_name][0] += self.count_of_sessions
-            data[self.user_name][1] = now
+            data[self.user_name]['session'] += self.count_of_sessions
+            data[self.user_name]['time'] = now
         else:
-                data[self.user_name] = {'Session': self.count_of_sessions, 'Time':now}
-            
+                data[self.user_name] = {'session': self.count_of_sessions, 'time':now}
+
         with open(self.history, 'w') as file:
             json.dump(data, file, indent=4)
+
+    def save_log(self):
+        data = self.check_log()
+        now = datetime.now().strftime("%Y-%m-%d")
+        duration = self.find_duration()
+        if self.user_name in data:
+            name = data[self.user_name]
+            name.append({'date': now,
+                         'start_time': self.start_time,
+                         'end_time': self.end_time,
+                         'duration': duration})
+        else:
+            data[self.user_name] = [{'date': now,
+                                     'start_time': self.start_time,
+                                     'end_time': self.end_time,
+                                     'duration': duration}]
+
+        with open(self.log, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    def find_duration(self):
+        h, m, s = map(int, self.start_time.split(':'))
+        t1 = timedelta(hours=h, minutes=m)
+        h, m, s = map(int, self.end_time.split(':'))
+        t2 = timedelta(hours=h, minutes=m)
+        if t2 < t1:
+            t2 += timedelta(days=1)
+        delta = t2 - t1
+        return int(delta.total_seconds() / 60)
 
     def session(self):
         is_running = True
 
         self.time_config()
         while is_running:
+            self.start_time = datetime.now().strftime("%H:%M:%S")
             self.job()
+            self.end_time = datetime.now().strftime("%H:%M:%S")
             self.count_of_sessions += 1  # one session was finished
             self.pause()
+            self.save_log()
             answer = input("Start another session? [y/n]: ").lower()
             is_running = answer == "y"
         self.save_history()
 
+
 if __name__ == '__main__':
-    s = Pomidor(input("Enter your name: "))
-    Pomidor.session(s)
+    user = Pomidor(input("Enter your name: "))
+    user.session()
     print('Bye!')
 
